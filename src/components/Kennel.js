3da8db43a8a8273af12ebe850cbd02c58c7cd360
Elyse Dawson <dawson.elyse@gmail.com>
2018-11-16 13:34:24 -0600

    Book4 Chapter 1 React exercise/practice

---
+++ src/components/Kennel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
+import LocationList from "./locations/LocationList"  // Import EmployeeList component
+
+export default class Kennel extends Component {
+    render() {
+        return (
+            <div>
+                <LocationList />
+                <EmployeeList />
+            </div>
+        );
+    }
+}
---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class LocationList extends Component {
+    render() {
+        return (
+            <section>
+                <h1>Kennel Locations</h1>
+                <div>
+                    <h4>Nashville North Location</h4>
+                    <h5>500 Puppy Way</h5>
+                    <h4>Nashville South Location</h4>
+                    <h5>250 Labradoodle Ave</h5>
+                </div>
+            </section>
+        );
+    }
+}
